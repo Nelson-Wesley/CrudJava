2788b403d37320a99497c10518b07a67cafe39e5
Nelson Wesley <nelsonwesleyramos@gmail.com>
2022-10-23 23:18:05 -0300

    Add files via upload

---
+++ CrudPessoa/Tabela pessoa.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `pessoa`.`pessoa` (
+  `idpessoa` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `idade` INT NOT NULL,
+  `uf` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idpessoa`));
